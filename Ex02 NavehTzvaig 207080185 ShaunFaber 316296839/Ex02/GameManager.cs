166f0e82f77ed15f23393d8ceeb6d18741592b72
fabershaun <fabershaun@gmail.com>
2025-05-11 13:00:11 +0300

    classes

---
+++ Ex02 NavehTzvaig 207080185 ShaunFaber 316296839/Ex02/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal class GameManager
+    {
+        public static void startGame()
+        {
+
+        }
+    }
+}
---
+++ Ex02 NavehTzvaig 207080185 ShaunFaber 316296839/Ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex02
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            GameManager.startGame();
+        }
+    }
+}
