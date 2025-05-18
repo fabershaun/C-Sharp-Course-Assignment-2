9824fb330c9a60cd721630c0bd9c230a050e7be2
fabershaun <fabershaun@gmail.com>
2025-05-18 15:02:52 +0300

    rename

---
+++ Mastermind-Game/Ex02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex02
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            GameManager game = new GameManager();
+            game.StartGame();
+        }
+    }
+}
